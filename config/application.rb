f0baf3c1a391818e55ddaf6dc2a6d4ea5d054c95
Mark Moeller <MarkMoellerUVA@gmail.com>
2018-08-07 18:11:25 -0400

    set up basic info

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
-# require "sprockets/railtie"
+require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -22,6 +22,12 @@ module ListOfIngredients
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # Middleware for ActiveAdmin
+    config.middleware.use Rack::MethodOverride
+    config.middleware.use ActionDispatch::Flash
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
@@ -31,5 +37,6 @@ module ListOfIngredients
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.app_generators.scaffold_controller = :scaffold_controller
   end
 end
